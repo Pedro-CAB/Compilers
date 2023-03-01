62eb8dd3bb14d68979ee0eb625ac18ddf568a90b
Pedro-CAB <up202006086@up.pt>
2023-03-01 11:54:21 +0000

    Minor changes for printing tree and input

--- src/main/pt/up/fe/comp2023/Launcher.java
+++ src/main/pt/up/fe/comp2023/Launcher.java
@@ -40,6 +40,9 @@ public class Launcher {
         // Check if there are parsing errors
         TestUtils.noErrors(parserResult.getReports());
 
+        //Prints the tree nodes
+        System.out.println(parserResult.getRootNode().toTree());
+
         // ... add remaining stages
     }
 
