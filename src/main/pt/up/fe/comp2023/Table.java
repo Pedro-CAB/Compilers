03f2edf5c40c787839871f7d248da64f3397b8e0
Pedro-PFerreira <up202004986@edu.fe.up.pt>
2023-03-15 12:29:04 +0000

    Finished Symbol Table, visitor and analysis left

--- src/main/pt/up/fe/comp2023/Table.java
+++ src/main/pt/up/fe/comp2023/Table.java
@@ -4,6 +4,7 @@ import pt.up.fe.comp.jmm.analysis.table.Symbol;
 import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
 import pt.up.fe.comp.jmm.analysis.table.Type;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Table implements SymbolTable {
@@ -11,21 +12,48 @@ public class Table implements SymbolTable {
     List<String> imports, methods;
 
     List<Symbol> fields, parameters, local_var;
-    String class_name, superClass; //classes
+    String class_name, super_class; //classes
 
-    Type return_type;
+    Type ret_type;
+
+    public Table(){
+        this.imports = new ArrayList<>();
+        this.methods = new ArrayList<>();
+        this.fields = new ArrayList<>();
+        this.parameters = new ArrayList<>();
+        this.local_var = new ArrayList<>();
+        this.class_name = "";
+        this.super_class = "";
+        this.ret_type = new Type("", false);
+    }
+
+    public void setImports(List<String> imports) {
+        this.imports = imports;
+    }
 
     @Override
     public List<String> getImports() {
         return imports;
     }
 
+    public void setClassName(String class_name) {
+        this.class_name = class_name;
+    }
+
     public String getClassName() {
         return class_name;
     }
 
+    public void setSuper(String superClass){
+        this.super_class = superClass;
+    }
+
     public String getSuper() {
-        return superClass;
+        return super_class;
+    }
+
+    public void setFields(List<Symbol> fields) {
+        this.fields = fields;
     }
 
     @Override
@@ -33,27 +61,37 @@ public class Table implements SymbolTable {
         return fields;
     }
 
+    public void setMethods(List<String> methods) {
+        this.methods = methods;
+    }
+
     @Override
     public List<String> getMethods() {
         return methods;
     }
 
+    public void setReturnType(Type ret_type) {
+        this.ret_type = ret_type;
+    }
 
     public Type getReturnType(String methodSignature) {
-
-        //TODO
-
-        return return_type;
+        return ret_type;
     }
+
+    public void setParameters(List<Symbol> parameters) {
+        this.parameters = parameters;
+    }
+
     public List<Symbol> getParameters(String methodSignature) {
 
-        //TODO
         return parameters;
     }
 
+    public void setLocalVariables(List<Symbol> local_var) {
+        this.local_var = local_var;
+    }
     public List<Symbol> getLocalVariables(String methodSignature){
 
-        //TODO
         return local_var;
     }
 }
---
+++ src/main/pt/up/fe/comp2023/TableVisitor.java
@@ -0,0 +1,99 @@
+package pt.up.fe.comp2023;
+
+import pt.up.fe.comp.jmm.analysis.table.Symbol;
+import pt.up.fe.comp.jmm.analysis.table.Type;
+import pt.up.fe.comp.jmm.ast.AJmmVisitor;
+import pt.up.fe.comp.jmm.ast.JmmNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TableVisitor extends AJmmVisitor<String, String> {
+
+    Table table = new Table();
+    @Override
+    protected void buildVisitor() {
+        addVisit("Imports", this::dealWithImports);
+        addVisit("Class Name", this::dealWithClassName);
+        addVisit("Super", this::dealWithSuper);
+        addVisit("Fields", this::dealWithFields);
+        addVisit("Methods", this::dealWithMethods);
+        addVisit("RetType", this::dealWithRetType);
+        addVisit("Parameters", this::dealWithParameters);
+        addVisit("LocalVar", this::dealWithLocalVars);
+
+    }
+
+    private String dealWithImports(JmmNode jmmNode, String s){
+
+        //TODO
+        List<String> imports = new ArrayList<>();
+        table.setImports(imports);
+        return "";
+    }
+
+    private String dealWithClassName(JmmNode jmmNode, String s){
+
+        //TODO
+
+        String class_name = "";
+
+        table.setClassName(class_name);
+
+        return "";
+    }
+
+    private String dealWithSuper(JmmNode jmmNode, String s){
+        //TODO
+
+        String sup = "";
+
+        table.setSuper(sup);
+
+        return "";
+    }
+
+    private String dealWithFields(JmmNode jmmNode, String s){
+        //TODO
+
+        List<Symbol> fields = new ArrayList<>();
+
+        table.setFields(fields);
+
+        return "";
+    }
+
+    private String dealWithMethods(JmmNode jmmNode, String s){
+        //TODO
+
+        List<String> methods = new ArrayList<>();
+
+        table.setMethods(methods);
+
+        return "";
+    }
+
+    private String dealWithRetType(JmmNode jmmNode, String s){
+        //TODO
+
+        Type ret_type = new Type("", false);
+
+        table.setReturnType(ret_type);
+
+        return "";
+    }
+
+    private String dealWithParameters(JmmNode jmmNode, String s){
+        //TODO
+        return "";
+    }
+
+    private String dealWithLocalVars(JmmNode jmmNode, String s){
+        //TODO
+        return "";
+    }
+
+
+
+
+}
